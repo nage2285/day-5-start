06fa35888b6bddb1155d194e50e95911a260696e
cliffordcoco <88732726+nage2285@users.noreply.github.com>
2021-08-11 04:40:00 +0000

    even number calculation

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
-#List 
-fruits = ["Apple", "Peach", "Pear"]
+#For Loop with Lists
+#fruits = ["Apple", "Peach", "Pear"]
 
 # Assigning of variable name
-for fruit in fruits:
-  print(fruit)
-  print(fruit + " Pie" )
+# Since there are three items in the list, for loop will run 3 times
+#for fruit in fruits:
+ # print(fruit)
+  #print(fruit + " Pie" )
+  #print(fruit + " chaat")
+  #print(fruits)
+
+#This will not be part of the for loop
+#print(fruits)
+
+#Using for loop with range function
+
+# Range Function (shows 1-9). Here 2 is the stepsize
+
+#for number in range(1, 11, 2):
+#  print(number)
+
+# Gauss adding first 100 numbers 
+total = 0
+for number in range(1,101):
+  total = total + number
+  #print(total)
+print(total)
+
+#adding even numbers in first 100 
+
+#Remember variable has to be defined intially
+total = 0
+for number in range(1, 101):
+  if (number % 2 == 0):
+    total = total + number
+print(total)
+
